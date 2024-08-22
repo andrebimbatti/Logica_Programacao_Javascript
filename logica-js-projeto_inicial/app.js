5b33487e045f145ccb339439a263df90faf958f7
Andre Bimbatti <andrebimbatti87@gmail.com>
2024-08-22 13:54:50 -0300

    Adicionado dicas

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,10 +1,14 @@
 {alert('Boas vindas ao jogo do número secreto')};
-let numeroSecreto = 4;
+let numeroSecreto = 5;
 console.log(numeroSecreto)
 let chute = prompt('Escolha um número entre 1 a 10');
 
 if (chute == numeroSecreto) {
     alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto}`);
 } else {
-    alert('Você errou :(')
+    if (chute < numeroSecreto){
+        alert('O número secreto é maior que ' + chute);
+    } else {
+        alert('O número é menor que ' + chute);
+    }
 }
