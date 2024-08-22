f2fc27cd76f33eba660a1e273515b7118199b36a
Andre Bimbatti <andrebimbatti87@gmail.com>
2024-08-22 00:37:05 -0300

    Adicionado Else e comentarios

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,7 +1,10 @@
 {alert('Boas vindas ao jogo do número secreto')};
 let numeroSecreto = 5;
+console.log(numeroSecreto)
 let chute = prompt('Escolha um número entre 1 a 10');
 
 if (chute == numeroSecreto) {
-    console.log('Isso aí! Você descobriu o número secreto!');
+    alert('Isso aí! Você descobriu o número secreto!');
+} else {
+    alert('Você errou :(')
 }
