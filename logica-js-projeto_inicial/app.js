e46d0917066c0e032aecf1aa33e6154761907e40
Andre Bimbatti <andrebimbatti87@gmail.com>
2024-08-22 14:09:33 -0300

    Adicionado Loop e contador de tentativas

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,14 +1,23 @@
 {alert('Boas vindas ao jogo do número secreto')};
 let numeroSecreto = 5;
-console.log(numeroSecreto)
-let chute = prompt('Escolha um número entre 1 a 10');
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
 
-if (chute == numeroSecreto) {
-    alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto}`);
-} else {
-    if (chute < numeroSecreto){
-        alert('O número secreto é maior que ' + chute);
+
+while (chute != numeroSecreto){
+    chute = prompt('Escolha um número entre 1 a 10');
+    
+    if (chute == numeroSecreto) {
+        alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ` + tentativas + ' tentativas');
     } else {
-        alert('O número é menor que ' + chute);
+        if (chute < numeroSecreto){
+            alert('O número secreto é maior que ' + chute);
+        } else {
+            alert('O número é menor que ' + chute);
+        }
+        tentativas ++;
     }
+
 }
+
