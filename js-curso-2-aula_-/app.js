b8e627c4cd6a1c3ac4382cfab1c015e76147bba9
Andre Bimbatti <andrebimbatti87@gmail.com>
2024-08-22 23:44:09 -0300

    Adicionado contador de tentativas e mensagens personalizadas

--- js-curso-2-aula_-/app.js
+++ js-curso-2-aula_-/app.js
@@ -1,4 +1,5 @@
 let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
 
 function exibirTextoNaTela(tag, texto){
     let campo = document.querySelector(tag);
@@ -12,13 +13,16 @@ function verificarChute() {
     let chute = document.querySelector('input').value;
     if (chute == numeroSecreto){
         exibirTextoNaTela('h1', 'Acertou!');
-        exibirTextoNaTela('p', 'Você descobriu o número secreto!');
+        let palavraTentiva = tentativas > 1 ? 'tentativas' : 'tentativa;'
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentiva}`;
+        exibirTextoNaTela('p', mensagemTentativas);
     } else {
         if(chute > numeroSecreto){
             exibirTextoNaTela('p', 'O número secreto é menor');
         } else{
             exibirTextoNaTela('p', 'O número secreto é maior');
         }
+        tentativas ++;
     }
 }
 
